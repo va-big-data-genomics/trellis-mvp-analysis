a2ba40840a2f700b1416801a121019921ae38165
Paul Billing-Ross <pbilling@stanford.edu>
2020-10-05 16:21:47 -0700

    Write completed notebooks to GCS

--- cronjobs/run-sample-based-analysis.sh
+++ cronjobs/run-sample-based-analysis.sh
@@ -4,6 +4,6 @@ source /home/jupyter/.bashrc
 
 papermill \
     /home/jupyter/notebooks/trellis_mvp_phase3_SampleBasedAnalysis.ipynb \
-    gs://${TRELLIS_BUCKET}/analysis-notebooks/trellis_mvp_phase3_SampleBasedAnalysis.ipynb \
+    gs://${TRELLIS_BUCKET}/analysis-notebooks/completed/trellis_mvp_phase3_SampleBasedAnalysis.ipynb \
     -p bucket_info ${TRELLIS_BUCKET} \
     -p credential_info trellis-config.yaml
