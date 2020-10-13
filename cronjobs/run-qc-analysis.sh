e4f1c18a93c6da36e01c34d64cde6aaf6a328322
Paul Billing-Ross <pbilling@stanford.edu>
2020-10-13 13:03:21 -0700

    Source bashrc file

--- cronjobs/run-qc-analysis.sh
+++ cronjobs/run-qc-analysis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PATH=/usr/bin:/bin:/usr/local/bin:/opt/conda/bin
+source /home/jupyter/.bashrc
 
 papermill \
     /home/jupyter/notebooks/trellis_mvp_phase3_QC.ipynb \
-    gs://${TRELLIS_BUCKET}/analysis-notebooks/trellis_mvp_phase3_QC.ipynb \
+    gs://${TRELLIS_BUCKET}/analysis-notebooks/completed/trellis_mvp_phase3_QC.ipynb \
     -p bucket_info ${TRELLIS_BUCKET} \
     -p credential_info trellis-config.yaml
