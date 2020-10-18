06c1514655f1b6f00ea553471d9bd02ac6f438c3
Paul Billing-Ross <pbilling@stanford.edu>
2020-10-17 21:51:23 -0700

    Moved cron jobs to Terraform

--- cronjobs/run-job-based-analysis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /home/jupyter/.bashrc
-
-papermill \
-    /home/jupyter/notebooks/trellis_mvp_phase3_JobBasedAnalysis.ipynb \
-    gs://${TRELLIS_BUCKET}/analysis-notebooks/completed/trellis_mvp_phase3_JobBasedAnalysis.ipynb \
-    -p bucket_info ${TRELLIS_BUCKET} \
-    -p credential_info trellis-config.yaml
--- cronjobs/run-sample-based-analysis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /home/jupyter/.bashrc
-
-papermill \
-    /home/jupyter/notebooks/trellis_mvp_phase3_SampleBasedAnalysis.ipynb \
-    gs://${TRELLIS_BUCKET}/analysis-notebooks/completed/trellis_mvp_phase3_SampleBasedAnalysis.ipynb \
-    -p bucket_info ${TRELLIS_BUCKET} \
-    -p credential_info trellis-config.yaml
